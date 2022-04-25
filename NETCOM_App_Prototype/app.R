e6c8a7a3b818512af3bc5a3f0178ce1c7c465360
jtmccorm <jtmccorm@andrew.cmu.edu>
2022-04-25 17:03:50 -0400

    [UI] Design Input functionalities; conditionals control access to "Developer Mode"

--- NETCOM_App_Prototype/app.R
+++ NETCOM_App_Prototype/app.R
@@ -3,6 +3,7 @@
 # Shiny
 library(shiny)
 library(shinydashboard)
+library(shinyWidgets)
 library(fresh)
 # AWS
 library(aws.s3)
@@ -42,6 +43,17 @@ read_all_csvs <- function(bucket, folder){
   }
 }
 
+normalize_SHAP <- function(scores, SHAP_df){
+  # Normaize (as in fits to normal distribution centered at zero) SHAP values
+  assertthat::are_equal(mean(scores), SHAP_df$baseline[1], tol=0.0001)
+  stn_dev <- sd(scores)
+  
+  SHAP_df %>%
+    mutate(across(.cols=everything(),
+                  .fns= ~ (.x/stn_dev))) %>%
+    select(-baseline)
+}
+
 ## B. Download files of interest --------------------
 ### I. Access Bucket -----------------------------
 bucket <- get_bucket(bucket="xstream-capstone")
@@ -49,12 +61,100 @@ bucket <- get_bucket(bucket="xstream-capstone")
 read_all_csvs(bucket, "data") 
 
 ## C. Clean and Prep data -------------------------
+### I. Create 'Display' Dataset ------------------
+# clean the primary 'display' data
+display_df <- enriched_altIP %>% select(-`...1`, -`Unnamed: 0`)
+#TODO  remove extraneous df's
+# remove(enriched_altIP, CleanEnrichedData)
 
+### II. Normalize SHAP Values from all  algorithms ----
+SHAPiforest <- normalize_SHAP(EnrichedScoreData$iforest_scores, SHAPiforest)
+SHAPlof <- normalize_SHAP(EnrichedScoreData$lof_scores, SHAPlof)
+SHAPxstream <- normalize_SHAP(EnrichedScoreData$xstream_scores, SHAPxstream)
+SHAPocsvm <- normalize_SHAP(EnrichedScoreData$ocsvm_scores, SHAPocsvm)
 
+#TODO  remove unneeded df 
+# remove(EnrichedScoreData)
+
+### III. Assign identifier to all rows of data ----
+SHAPiforest['INDICATOR'] <- display_df['INDICATOR']
+SHAPxstream['INDICATOR'] <- display_df['INDICATOR']
+SHAPlof['INDICATOR']     <- display_df['INDICATOR']
+SHAPocsvm['INDICATOR']   <- display_df['INDICATOR']
+
+print('Data Prep Complete')
 
 # 1. User Interface ===================================================
 
-## A. Create UI Theme -------------------------------------------------
+## A. Helper Fxns -----------------------------------------------------
+
+feature_panel <- function(model_title){
+  # Function to procedurally generate inputs for each model
+  # first pull all feature names
+  feat_names <- dplyr::select(SHAPiforest, -INDICATOR) %>%
+    names() %>% 
+    str_remove_all(., "__.*$") %>%
+    unique()
+  feat_1 <- split(feat_names, rep(c(1,2), 9))$`1`
+  feat_2 <- split(feat_names, rep(c(1,2), 9))$`2`
+  
+  # build the feature panel
+  conditionalPanel(
+    condition = str_c("input.model == '", model_title,"'"),
+    h4(str_c(model_title, ": Feature Weights"), align = 'center'),
+    
+    column(width=6,
+           chooseSliderSkin("Flat", color = "#2E3440"),
+           lapply(feat_1, function(feat) {
+             sliderInput(inputId = str_c(model_title,"_",feat),
+                         label = feat,
+                         min = 0, max = 1.5, value = 1,
+                         step = 0.25, ticks=F)
+           })),
+    column(width=6,
+           chooseSliderSkin("Flat", color = "#2E3440"),
+           lapply(feat_2, function(feat) {
+             sliderInput(inputId = str_c(model_title,"_",feat),
+                         label = feat,
+                         min = 0, max = 1.5, value = 1,
+                         step = 0.25, ticks=F)
+           })
+    )
+  )
+}
+
+model_switch <- function(model, status){
+  # creates shinyWidget::materialSwitch for models
+  materialSwitch(
+    inputId = str_c(model, "_include"),
+    label = model, 
+    value = TRUE,
+    status = status)
+}
+
+eda_ui <- function(width){
+  # function to allow the EDA UI to dynamically respond to developer mode
+  column(width = width,
+         fluidRow(
+           # Inputs
+           box(width=4, selectInput("plotType",
+                                    "Distribution to Explore",
+                                    choices = c("Univariate", "Bivariate"),
+                                    selected = "Univariate"),
+           ),
+           # Explanation
+           box(width=8, 
+               h4(strong("Evaluate and Modify Models"), align = 'center'),
+               p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."))
+         ),
+         # Plot 
+         fluidRow(
+           box(width = 12,
+               h4(strong("Some Plot"), align='center')))
+  )
+}
+
+## B. Create UI Theme -------------------------------------------------
 # drawn from example at https://github.com/dreamRs/fresh
 my_theme <- create_theme(
   adminlte_color(
@@ -72,31 +172,182 @@ my_theme <- create_theme(
   )
 )
 
-## B. Application Header-----------------------------------------------
-header <- dashboardHeader(title = "Anomaly Dashboard")
+## C. Application Header-----------------------------------------------
+header <- dashboardHeader(title = "Anomaly Detection")
 
-## C. Dashboard Sidebar------------------------------------------------
+## D. Dashboard Sidebar------------------------------------------------
 sidebar <-dashboardSidebar(
   sidebarMenu(
     id='tabs',
-    ### I. Menu Items ------------------------------------------------
+    ### I. Tab Selections ------------------------------------------------
     menuItem("Explore Anomalies", icon=icon("eye"), tabName = "ranks"),
-    menuItem("Evaluate Models", icon=icon("line-chart", tabName="models")),
-    menuItem("Full Dataset", icon=icon("table"), tabName = "table")
+    menuItem("Evaluate Models", icon=icon("line-chart"), tabName="eval"),
+    menuItem('About', icon = icon("question"), tabName = 'about'),
+    
+    ### II. Developer Access  -----------------------------------------
+    box(width = 12,
+        background = 'navy',
+        # Header for section
+        h4(strong("Developer Access"), align = 'center'),
+        
+        # credentials text inputs
+        p("Enter your credentials below:"),
+        
+        textInput("user","Username"),
+        
+        passwordInput("password", "Password"),
+        
+        actionBttn("dev_cred", "Enter", 
+                   style = 'simple'),
+        
+        # Responds to button via observe event
+        fluidRow(align = 'center',
+          textOutput("dev_display"),
+          # developer mode switch
+          conditionalPanel(
+            condition = "output.dev_display == 'Access Granted!'",
+            switchInput(
+              inputId = "dev_access",
+              label = strong("Developer Mode"),
+              onStatus = "success", 
+              offStatus = "danger",
+              labelWidth = "100px"
+              )
+          )
+        )
+      )
   )
 )
 
-## D. Dashboard Body---------------------------------------------------
+## E. Dashboard Body---------------------------------------------------
 body <- dashboardBody(use_theme(my_theme),
-             )
+        tabItems(
+          ### I. Explore Anomaly Tab -----------------------------------
+          tabItem("ranks",
+                  fluidPage(
+                    #### a. Ranked Anomaly Table -------------
+                    column(width =7,
+                           box(width=12,
+                               h4(strong("Ranked Anomalies"), align='center'))
+                    ),
+                    column(width=5,
+                     
+                      #### b. SHAP plots -----------------------
+                      box(width =12,
+                          h4(strong("Feature Significance"), align='center'), 
+                          tabBox(width=12,
+                             tabPanel("iForest"),
+                             tabPanel('xStream')
+                                )
+                          ),
+                      #### c. Action buttons --------------------------
+                      box(width=12, align = 'center',
+                          h4(strong("Action Steps"), align ='center'),
+                          p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+                          actionBttn('mark', label="Investigate",
+                                     style = "unite",
+                                     color = 'danger'),
+                          actionBttn('ignore', label="Ignore",
+                                     style = "unite",
+                                     color = 'warning')
+                          )
+                        )
+                      )
+                    ),
+          ### II. Model Evaluation -------------------------------------
+          tabItem("eval",
+                  fluidPage(
+                    #### a. EDA Plots-------------------
+                    # dynamically rebuild to provide room for model controls
+                    uiOutput("eda_ui"),
+                    #### b. Model Controls -------------------
+                    # must be conditional panel to ensure inputs always exist (just hidden)
+                    conditionalPanel( 
+                      condition = "input.dev_access == true",
+                      column(width = 5,
+                          box(width=12, 
+                            h4(strong("Model Controls"), align='center'),
+                            ##### i. Ensemble Controls ----
+                            p("Choose which models will be retained in the final ensemble:"),
+                            fluidRow(
+                              column(width=2),
+                              column(width = 4, 
+                                     model_switch("iForest","success"),
+                                     model_switch("xStream","danger")),
+                              column(width = 4,
+                                     model_switch("LOF", "info"),
+                                     model_switch("OCSVM", "warning")),
+                              column(width=2)
+                            ),
+                            ##### ii. Model Selection -------
+                            p("Select a model below to modify the weights ansigned to each feature."),
+                            fluidRow(align='center',
+                              pickerInput('model', 
+                                          label = 'Model: ',
+                                          choices = c('iForest', 'xStream',
+                                                      'LOF', 'OCSVM', 'None'),
+                                          selected = 'None',
+                                          choicesOpt = list(
+                                            icon = c("glyphicon-tree-conifer", 
+                                                     "glyphicon-random", 
+                                                     "glyphicon-sunglasses", 
+                                                     "glyphicon-dashboard", 
+                                                     "glyphicon-option-horizontal")
+                                          ),
+                                          width = '60%',
+                                          inline=TRUE)
+                              ),
+                          ##### iii. Feature Weight Selection ----
+                            feature_panel('iForest'),
+                            feature_panel('xStream'),
+                            feature_panel('LOF'),
+                            feature_panel('OCSVM')
+                          )
+                          )
+                    )
+                      )
+                  ),
+          ### III. About -------------------------------------------------
+          tabItem("about",
+                  box(width =7,
+                      h4(strong('About'), align='center'),
+                      tabBox(width=12, 
+                             tabPanel("Project",
+                                      p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")),
+                             tabPanel("Contributors",
+                                      p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")))
+                      )
+                  )
+        )
+)
 
-## E. UI Setup---------------------------------------------------------
-ui <- dashboardPage(header, sidebar, body)
 
-# Define server logic required to draw a histogram
+## F. UI Setup---------------------------------------------------------
+ui <- dashboardPage(header, sidebar, body,
+                    title = "NETCOM-DSD: Anomaly Detection")
+
+# 2. Server ===============================================
 server <- function(input, output) {
-
-
+  # EDA User Interface updates
+  output$eda_ui <- renderUI({
+    if (input$dev_access){
+      eda_ui(7)
+    } else{
+      eda_ui(10)
+    }
+  })
+  
+  dev_cred <- eventReactive(input$dev_cred,{
+    input$user == "slartibartfast" & input$password == "42"
+  })
+  
+  output$dev_display <- renderText({
+    if (dev_cred()){
+      "Access Granted!"
+    } else{
+      "Access Denied."
+    }
+  })
 }
 
 # Run the application 
